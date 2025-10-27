8f948940883db5385211b960e42c26b856c6b4d7
Сливная Инна <107200768+Inna1Sliva@users.noreply.github.com>
2025-10-27 15:01:24 +0300

    add: ScreenProfileNavigation.kt

---
+++ feature_main/src/main/java/com/it/shka/feature_main/presentation/navigation/ScreenProfileNavigation.kt
@@ -0,0 +1,21 @@
+package com.it.shka.feature_main.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.it.shka.feature_main.presentation.route.RoutScreenProfile
+import com.it.shka.feature_main.presentation.screens.profileinfo.ScreenProfile
+
+@Composable
+fun ScreenProfileNavigation(){
+val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = RoutScreenProfile.ScreenProfile.rout
+    ){
+        composable(RoutScreenProfile.ScreenProfile.rout){
+            ScreenProfile()
+        }
+    }
+}
